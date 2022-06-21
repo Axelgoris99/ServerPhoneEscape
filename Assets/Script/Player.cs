60a5a0bcb80d08b313891c8f713e60c4c1687f1b
Axel Goris <axelgoris99@gmail.com>
2022-06-21 15:23:00 +0200

    Feat: Finished Tutorial

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,7 +5,10 @@ using Mirror;
 
 public class Player : NetworkBehaviour
 {
+    [SyncVar(hook = nameof(OnHolaCountChanged))]
+    int holaCount = 0;
     public float speed = 0.1f;
+
     void HandleMovement()
     {
         if(isLocalPlayer)
@@ -20,5 +23,40 @@ public class Player : NetworkBehaviour
     private void Update()
     {
         HandleMovement();
+        if (isLocalPlayer && Input.GetKeyDown("x"))
+        {
+            Debug.Log("Sending Hola to Server");
+            Hola();
+        }
+    }
+
+    public override void OnStartServer()
+    {
+        Debug.Log("Player spawned ");
+    }
+
+    [Command]
+    void Hola()
+    {
+        Debug.Log("Received Hola from Client");
+        holaCount += 1;
+        ReplyHola();
+    }
+
+    [TargetRpc]
+    void ReplyHola()
+    {
+        Debug.Log("Reply Hola to Server");
+    }
+
+    [ClientRpc]
+    void TooHigh()
+    {
+        Debug.Log("Too High!");
+    }
+
+    void OnHolaCountChanged(int oldCount, int newCount)
+    {
+        Debug.Log($"We had {oldCount} holas and now we have {newCount} holas");
     }
 }
